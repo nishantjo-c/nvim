d29bfd63198a29003a8acb8f5c5799bc17f94e28
nishant <nishant@akitra.com>
2024-06-01 10:14:56 +0530

    added definition

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,11 +1,36 @@
 return {
   {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup {
+        ensure_installed = { 
+          "lua_ls", 
+          "rust_analyzer",
+          "tsserver"
+        },
+      }
+    end
+  },
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    config = function()
+      local lsp_zero = require("lsp-zero")
+      lsp_zero.on_attach(function(client, bufnr)
+        lsp_zero.default_keymaps({buffer = bufnr})
+      end)
+    end
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
-        
-     -- lspconfig.tsserver.setup({})
-
+      local lspconfig = require("lspconfig") 
+      lspconfig.tsserver.setup({})
       lspconfig.lua_ls.setup({
         settings = {
           Lua = {
@@ -21,22 +46,4 @@ return {
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, { desc = "LSP: Code action" })
     end
   },
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup {
-        ensure_installed = { 
-          "lua_ls", 
-          "rust_analyzer",
-         -- "tsserver"
-        },
-      }
-    end
-  },
-  }
+}
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,14 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-     branch = "v3.x",
-     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal right<CR>')
-    end
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal right<CR>')
+    vim.keymap.set('n', '<leader>x', ':Neotree close<CR>')
+  end
 }
+
